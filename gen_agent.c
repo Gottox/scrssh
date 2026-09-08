64f36b92a6161ddad0938d5faf76ffc177666457
Enno Boland <g@s01.de>
2026-09-08 09:24:28 +0200

    Embed a minified agent when pyminify is available

--- gen_agent.c
+++ gen_agent.c
@@ -2,34 +2,19 @@
  * Copyright (C) 2026 Enno Boland <g@s01.de>
  */
 
-#define _GNU_SOURCE
 #include <assert.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 int
 main(void) {
-	char *line = NULL, *p;
-	size_t n = 0;
-	puts("'\\'',");
-	while (getline(&line, &n, stdin) > 0) {
-		assert(strchr(line, '\'') == NULL);
-		if ((p = strchr(line, '#'))) {
-			*p = 0;
-		}
-		for (p = &line[strlen(line) - 1]; p >= line && strchr(" \t\n", *p);
-			 p--) {
-			*p = 0;
-		}
-		for (p = line; *p && *p != '\n' && *p != '#'; p++) {
-			printf("%d,", *p);
-		}
-		if (p != line) {
-			puts("'\\n',");
-		}
+	int squote = 0, dquote = 0;
+	fputs("'\\\'',", stdout);
+	for (int byte; (byte = getchar()) != EOF;) {
+		squote |= byte == '\'';
+		dquote |= byte == '"';
+		printf("%d,", byte == '\'' ? '"' : byte);
 	}
-	puts("'\\'',0\n");
-	free(line);
+	assert(!squote || !dquote);
+	fputs("'\\\'',0", stdout);
 	return 0;
 }
